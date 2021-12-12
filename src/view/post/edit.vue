c4742de6af65c8af91743398e7b658874bcf2808
peng49 <1219955253@qq.com>
2021-12-12 23:50:18 +0800

    update edit.vue

--- src/view/post/edit.vue
+++ src/view/post/edit.vue
@@ -1,40 +1,49 @@
 <template>
   <el-row>
     <el-col :span="2">
-        <a href="">文章管理</a>
+      <a href="">文章管理</a>
     </el-col>
     <el-col :span="20">
       <el-input v-model="post.title" placeholder="请输入文章标题" />
     </el-col>
   </el-row>
-  <div id="editor" style="min-height:700px"><textarea></textarea></div>
+  <el-row>
+    <el-col :span="24" style="min-height: 700px">
+        <div id="editor"><textarea></textarea></div>
+    </el-col>
+  </el-row>  
 </template>
 
 <script>
 import jQuery from "jquery";
-const $ = window.$ = window.jQuery = jQuery
+const $ = (window.$ = window.jQuery = jQuery);
 
 export default {
   data() {
     return {
+      editor: null,
       post: { title: "" },
     };
   },
   mounted() {
+    let _this = this;
     // https://blog.csdn.net/jdbdh/article/details/90314447
-    $.getScript("/static/editor.md/editormd.min.js",function(s){
-                console.log(s)
-                // eslint-disable-next-line no-undef
-                editormd('editor',{
-                  path:'/static/editor.md/lib/',
-                   saveHTMLToTextarea: true,
-                height: "100%",
-                watch: true,
-                })
-    })
+    $.getScript("/static/editor.md/editormd.min.js", function () {
+      _this.editor = window.editormd("editor", {
+        path: "/static/editor.md/lib/",
+        saveHTMLToTextarea: true,
+        height: "100%",
+        watch: true,
+        placeholder:'请输入内容'
+      });
+    });
     //加载css
-    $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '/static/editor.md/css/editormd.min.css') );
-
+    $("head").append(
+      $('<link rel="stylesheet" type="text/css" />').attr(
+        "href",
+        "/static/editor.md/css/editormd.min.css"
+      )
+    );
   },
 };
 </script>
