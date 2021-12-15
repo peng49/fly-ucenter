7ca11f3784e726cc6303ded8afa292607d016d97
peng49 <1219955253@qq.com>
2021-12-15 15:22:01 +0800

    update edit.vue

--- src/view/post/edit.vue
+++ src/view/post/edit.vue
@@ -9,18 +9,12 @@
   </el-row>
   <el-row style="height: 85vh">
     <el-col :span="24">
-        <div id="editor"><textarea>内容</textarea></div>
+      <div id="editor"><textarea>内容</textarea></div>
     </el-col>
-  </el-row>  
+  </el-row>
 </template>
 
-<style>
-.editormd-menu li a::after{
-  content: attr(title);
-  font-size: 12px;
-  display: block;
-}
-</style>
+
 
 <script>
 import jQuery from "jquery";
@@ -42,32 +36,44 @@ export default {
         saveHTMLToTextarea: true,
         height: "100%",
         watch: true,
-        placeholder:'请输入内容',
-        onload:function(){
+        placeholder: "请输入内容",
+        onload: function () {
           //重写样式
-          let styleCode = '';
+          let styleCode = `.editormd-menu li a i::after{
+            display: block;
+            font-size: 12px;
+            font-weight: 100;
+            margin-top: 14px;
+          }`;
 
-          `{
-              content: '内容';
-              display: block;
-              font-size: 12px;
-              font-weight: 100;
-              margin-top: 14px;
-          }`
-          console.log($('.editormd-menu li a'))
-          let icons = $('.editormd-menu li a')
-          icons.each(i => {
-            let cls = icons.eq(i).find('i').attr('class')
-            let title = icons.eq(i).attr('title')
-            let name = icons.eq(i).find('i').attr('name')
-            console.log('.editormd-menu li a i.'+cls.replace(' ','.')+'[name='+name+']::after',title,styleCode)
-          })
+          let style = document.createElement("style");
 
-        }
+          let icons = $(".editormd-menu li a");
+          icons.each((i) => {
+            let cls = icons.eq(i).find("i").attr("class").replace(" ", ".");
+            let title = icons.eq(i).attr("title");
+            let name = icons.eq(i).find("i").attr("name");
+
+            let map = {
+              ucwords: "大写",
+              uppercase: "大写",
+              lowercase: "小写",
+            };
+            title = map[name] ? map[name] : title;
+
+            styleCode += `.editormd-menu li a i.${cls}[name=${name}]::after{content:'${title}'}`;
+          });
+
+          style.innerHTML = styleCode;
+          $("head").append(style);
+          $(window).resize()
+        },
       });
     });
     //加载css
-    $("head").append('<link rel="stylesheet" type="text/css" href="/static/editor.md/css/editormd.min.css" />');
+    $("head").append(
+      '<link rel="stylesheet" type="text/css" href="/static/editor.md/css/editormd.min.css" />'
+    );
   },
 };
 </script>
